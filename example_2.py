adc1cc9dfd6713e7a6c4ef307944a34c08e0b37a
dr-zool <dr-zool>
2020-10-25 23:50:14 -0700

    remove README.md file

--- example_2.py
+++ example_2.py
@@ -1,7 +1,6 @@
 age = int(input("Enter your age in years: "))
 max_heart_rate = 206.9 - (0.67*age)
 target = 0.65*max_heart_rate
-#target = 0.5*max_heart_rate
 print("Target fat-burning heart rate:", target)
 
 
