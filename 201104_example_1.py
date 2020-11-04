81bb9fbd0a933372ca8795fcd1896eb33dfb1506
Zool H Ismail <37915963+dr-zool@users.noreply.github.com>
2020-11-03 17:40:49 -0800

    Adding a new example with break statement
    Debug the following code

---
+++ 201104_example_1.py
@@ -0,0 +1,17 @@
+#get an initial guess 
+guess str = input("Guess a number: ") 
+guess = (guess_str)  
+ 
+# while guess is range, keep asking 
+while 0 <= guess <= 100: 
+if guess > number: 
+print ("Guessed Too High.") 
+elif guess < number: 
+print("Guessed Too Low.") 
+else: # correct guess, exit with break was: ",number) 
+print("You guessed it. The number 
+# keep going, get the next guess 
+guess str = input("Guess a number: ") 
+guess = int(guess_str) 
+else: 
+print ("You quit early, the number was:",number) 
